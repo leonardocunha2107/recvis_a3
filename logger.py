d6166f0a86337e19a7e02c0515aa0cd687960d06
Leonardo Cunha <leonardocunha2107@gmail.com>
2020-11-13 18:30:49 +0100

    ready for efficient net

--- logger.py
+++ logger.py
@@ -1,3 +1,4 @@
+
 import torch
 from torch.utils.tensorboard import SummaryWriter
 import os.path as path
@@ -5,45 +6,30 @@ import shutil
 import numpy as np
 import json
 
-
-SEP='-'*40
-
 class Jimmy:
     def __init__(self,experiment_name,metric_keys,logdir='.',verbose=5):
         self.metric_keys=set(['train_'+k for k in metric_keys]+['val_'+k for k in metric_keys])
         self.verbose=verbose
 
-        self.log={k:[] for k in self.metric_keys]}
-        self.last_epoch={k:0 for k in self.metric_keys]}
+        self.log={k:[] for k in self.metric_keys}
+        self.last_epoch={k:0 for k in self.metric_keys}
         self.t={k:0 for k in self.metric_keys}
         self.model_t=0
         self.epoch=1
         self.logdir=logdir
-        config_item={'exp_name':experiment_name}
-        config_path=path.join(logdir,'logger_config.json')
-        if path.exists(config_path):
-            with open(config_path) as fd:
-                config_dict=json.load(fd)
-            exp_id=int(list(config_dict.keys())[-1])+1
-        else:
-           exp_id=1 
-           config_dict=dict()
-        config_dict[exp_id]=config_item
-        with open(config_path,'w+') as fd:
-            json.dump(config_dict,fd,indent=2)
+ 
         
-        self.tag=str(exp_id)
+        self.tag=experiment_name
         self.summ_path=path.join(logdir,self.tag)  
         self.sw=SummaryWriter(self.summ_path)
     
     def update(self, dic):
         assert all([k in self.metric_keys for k in dic.keys()])
         for k,v in dic.items():
-        if  torch.isnan(v):
-            print("NAN")#raise Exception('nan')
-        v=float(v)
-        self.t[k]+=1
-        self.sw.add_scalar(k,v,self.t[k])
+            v=float(v)
+            self.t[k]+=1
+            self.sw.add_scalar(k,v,self.t[k])
+            self.log[k].append(v)
     
     def close_epoch(self):
         pri=False
@@ -61,4 +47,5 @@ class Jimmy:
         self.sw.close()
         torch.save({'state_dict':model.state_dict(),
                     'log':self.log}, path.join(self.summ_path,'mdl.pt'))
+     
             
--- main.py
+++ main.py
@@ -14,57 +14,7 @@ import matplotlib.pyplot as plt
 from logger import Jimmy
 SEP='-'*40
 
-# Training settings
-parser = argparse.ArgumentParser(description='RecVis A3 training script')
-parser.add_argument('--data', type=str, default='bird_dataset', metavar='D',
-                    help="folder where data is located. train_images/ and val_images/ need to be found in the folder")
-parser.add_argument('--batch-size', type=int, default=64, metavar='B',
-                    help='input batch size for training (default: 64)')
-parser.add_argument('--epochs', type=int, default=10, metavar='N',
-                    help='number of epochs to train (default: 10)')
-parser.add_argument('--lr', type=float, default=0.1, metavar='LR',
-                    help='learning rate (default: 0.01)')
-parser.add_argument('--momentum', type=float, default=0.5, metavar='M',
-                    help='SGD momentum (default: 0.5)')
-parser.add_argument('--seed', type=int, default=1, metavar='S',
-                    help='random seed (default: 1)')
-parser.add_argument('--log-interval', type=int, default=10, metavar='N',
-                    help='how many batches to wait before logging training status')
-parser.add_argument('--experiment', type=str, default='experiment', metavar='E',
-                    help='folder where experiment outputs are located.')
-args = parser.parse_args()
-use_cuda = torch.cuda.is_available()
-torch.manual_seed(args.seed)
-
-# Create experiment folder
-if not os.path.isdir(args.experiment):
-    os.makedirs(args.experiment)
-
-# Data initialization and loading
-from data import data_transforms
-
-train_loader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(args.data + '/train_images',
-                         transform=data_transforms),
-    batch_size=args.batch_size, shuffle=True, num_workers=1)
-val_loader = torch.utils.data.DataLoader(
-    datasets.ImageFolder(args.data + '/val_images',
-                         transform=data_transforms),
-    batch_size=args.batch_size, shuffle=False, num_workers=1)
-
-# Neural network and optimizer
-# We define neural net in model.py so that it can be reused by the evaluate.py script
-from model import Net
-model = Net()
-if use_cuda:
-    print('Using GPU')
-    model.cuda()
-else:
-    print('Using CPU')
-
-optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
-logger=Jimmy('Charles',['loss','acc'],'logs')
-def train(epoch):
+def train(model,train_loader,oprimizer):
     model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         if use_cuda:
@@ -77,34 +27,76 @@ def train(epoch):
         optimizer.step()
         logger.update({'train_loss':loss})
 
+        with torch.no_grad():
+            pred = output.data.max(1, keepdim=True)[1]
+            logger.update({'train_acc': pred.eq(target.data.view_as(pred)).cpu().sum()})
 
-def validation():
+
+def validation(model,logger,val_loader):
     model.eval()
     validation_loss = 0
     correct = 0
-    for data, target in val_loader:
-        if use_cuda:
-            data, target = data.cuda(), target.cuda()
-        output = model(data)
-        # sum up batch loss
-        criterion = torch.nn.CrossEntropyLoss(reduction='mean')
-        validation_loss += criterion(output, target).data.item()
-        # get the index of the max log-probability
-        pred = output.data.max(1, keepdim=True)[1]
-        correct += pred.eq(target.data.view_as(pred)).cpu().sum()
-        logger.update({'val_loss':validation_loss})
+    with torch.no_grad():
+        for data, target in val_loader:
+            if use_cuda:
+                data, target = data.cuda(), target.cuda()
+            output = model(data)
+            # sum up batch loss
+            criterion = torch.nn.CrossEntropyLoss(reduction='mean')
+            validation_loss += criterion(output, target).data.item()
+            # get the index of the max log-probability
+            pred = output.data.max(1, keepdim=True)[1]
+            correct += pred.eq(target.data.view_as(pred)).cpu().sum()
+            logger.update({'val_loss':validation_loss})
 
-    validation_loss /= len(val_loader.dataset)
+        validation_loss /= len(val_loader.dataset)
     print('\nValidation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)'.format(
         validation_loss, correct, len(val_loader.dataset),
         100. * correct / len(val_loader.dataset)))
     logger.update({'val_acc':100. * correct / len(val_loader.dataset)})
+    
+if __name__='__main__':
+        # Training settings
 
-for epoch in range(1, args.epochs + 1):
-    train(epoch)
-    validation()
-    logger.close_epoch()
-    model_file = args.experiment + '/model_' + str(epoch) + '.pth'
-    #torch.save(model.state_dict(), model_file)
-    print('Saved model to ' + model_file + '. You can run `python evaluate.py --model ' + model_file + '` to generate the Kaggle formatted csv file\n')
-logger.close()
+    args = parser.parse_args()
+    use_cuda = torch.cuda.is_available()
+    torch.manual_seed(args.seed)
+
+    # Create experiment folder
+    if not os.path.isdir(args.experiment):
+        os.makedirs(args.experiment)
+
+    # Data initialization and loading
+    from data import data_transforms
+
+    train_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(args.data + '/train_images',
+                            transform=data_transforms),
+        batch_size=args.batch_size, shuffle=True, num_workers=1)
+    val_loader = torch.utils.data.DataLoader(
+        datasets.ImageFolder(args.data + '/val_images',
+                            transform=data_transforms),
+        batch_size=args.batch_size, shuffle=False, num_workers=1)
+
+    # Neural network and optimizer
+    # We define neural net in model.py so that it can be reused by the evaluate.py script
+    from model import Net
+    model = Net()
+    if use_cuda:
+        print('Using GPU')
+        model.cuda()
+    else:
+        print('Using CPU')
+
+    optimizer = optim.SGD(model.parameters(), lr=args.lr, momentum=args.momentum)
+    logger=Jimmy('Charles',['loss','acc'],'logs')
+
+
+    for epoch in range(1, args.epochs + 1):
+        train(epoch)
+        validation(model,logger,val_loader)
+        logger.close_epoch()
+        model_file = args.experiment + '/model_' + str(epoch) + '.pth'
+        #torch.save(model.state_dict(), model_file)
+        print('Saved model to ' + model_file + '. You can run `python evaluate.py --model ' + model_file + '` to generate the Kaggle formatted csv file\n')
+    logger.close()
